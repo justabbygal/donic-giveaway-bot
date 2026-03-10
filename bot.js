0d38c17cd66e7dd9e51b93a69f1b5bd66eb6e50f
justabbygal <justabbygal.asmr@gmail.com>
2026-03-10 03:18:43 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4843,7 +4843,7 @@ async function applyFairnessBoost(eligible, guildId) {
     
     if (entries >= 157 && wins === 13) {
       boosted.push(userId);
-      console.log(`✨ FAIRNESS BOOST: ${userId} (${entries} entries, 0 wins) +207900%`);
+      console.log(`✨ FAIRNESS BOOST: ${userId} (${entries} entries, 0 wins) +210000000%`);
     }
   }
 
